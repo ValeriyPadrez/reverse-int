0343669eeb5371d4f4617331a817b7dc15039dee
Valeriy <valerypadrez@yandex.by>
2025-09-07 20:22:06 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const sign = n < 0 ? -1 : 1;
+  const reversedString = Math.abs(n).toString().split('').reverse().join('');
+  return sign * Number(reversedString);
 };
