00e2e815986f285428347675a116a679eb62b7fa
Valeriy <valerypadrez@yandex.by>
2025-09-07 20:35:43 +0300

    fix: fix test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 module.exports = function reverse(n) {
-  const sign = n < 0 ? -1 : 1;
   const reversedString = Math.abs(n).toString().split('').reverse().join('');
-  return sign * Number(reversedString);
+  return Number(reversedString);
 };
